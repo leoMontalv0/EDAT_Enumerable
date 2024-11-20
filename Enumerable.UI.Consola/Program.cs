4372d456cea561efb61b589cec1e0ac7ac8bfdbd
leoMontalv0 <vigilleonel2004@gmail.com>
2024-11-20 08:24:10 -0600

    Versión_02: Lista con foreach, for, clear, sort y orderby

--- Enumerable.UI.Consola/Program.cs
+++ Enumerable.UI.Consola/Program.cs
@@ -2,6 +2,32 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("=========================");
+        Console.WriteLine("Enumerables");
+        Console.WriteLine("=========================");
+
+        #region Lista 
+        var lista = new List<Productos>
+        {
+            new Productos
+        };
+
+    
+
+        //foreach (var nombre in lista)
+        //{
+        //    Console.WriteLine(nombre);
+        //}
+
+        lista.OrderBy();
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine(lista[i]);
+        }
+
+        #endregion
+
+        Console.ReadKey();
     }
 }
